cc3be1e2e877db0ee6276e84c19ea74a8d542a5a
simaocunha71-miei <simaopscunha@outlook.pt>
2020-03-11 15:37:51 +0000

    identaçao e comentarios de interface.h (camada de interface)

--- git/interface.h
+++ git/interface.h
@@ -1,7 +1,8 @@
 #ifndef ___INTERFACE_H___
 #define ___INTERFACE_H___
 
-int interpretador(ESTADO *e);
-void mostrar_tabuleiro(ESTADO estado);
+
+int interpretador(ESTADO *e);           //Função que desenvolve o jogo;
+void mostrar_tabuleiro(ESTADO estado);  //Função que mostra o tabuleiro com o estado atual.
 
 #endif //___INTERFACE_H___
