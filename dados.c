6baeca3e93784ee0661e16b192835793a70d26b8
Gerimias <a93277@alunos.uminho.pt>
2020-03-11 12:10:40 +0000

    first commit

---
+++ dados.c
@@ -0,0 +1,21 @@
+typedef enum {VAZIO, BRANCA, PRETA} CASA;
+
+typedef struct
+{ int coluna;
+  int linha; }
+  COORDENADA;
+
+typedef struct
+{ COORDENADA jogador1;
+  COORDENADA jogador2; }
+  JOGADA;
+
+typedef JOGADA JOGADAS[32];
+
+typedef struct
+{ CASA tab[8][8]; // armazena informação sobre o tabuleiro;
+  COORDENADA ultima_jogada; // a coordenada da última jogada;
+  JOGADAS jogadas; // armazena informação sobre as jogadas;
+  int num_jogadas; //indica quantas jogadas foram efetuadas;
+  int jogador_atual; } //indica qual é o jogador a jogar;
+  ESTADO;
