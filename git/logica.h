6cb3aed3e8d24cb9ec0b113bea6260fa89cebf00
simaocunha71-miei <simaopscunha@outlook.pt>
2020-03-11 15:39:44 +0000

    identaçao e comentarios de logica.h (logica do programa)

--- git/logica.h
+++ git/logica.h
@@ -3,7 +3,7 @@
 
 #include "dados.h"
 
-int jogar(ESTADO *e, COORDENADA c);
-int acabar (ESTADO *e);
+int jogar(ESTADO *e, COORDENADA c); //Função que realiza cada jogada;
+int acabar (ESTADO *e);             //Função boleana que verifica se o jogo acabou.
 
 #endif //___LOGICA_H___
