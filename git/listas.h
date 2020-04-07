71c4009354c9247517413b24c72fb14fe23c9106
AdelioJoseFerreiraFernandes <a78778@alunos.uminho.pt>
2020-04-07 17:10:33 +0100

    update documentação listas.h

--- git/listas.h
+++ git/listas.h
@@ -1,9 +1,16 @@
+/**
+@file listas.h
+Módulo de listas ligadas do jogo e das funções que o manipulam
+*/
 
 #ifndef ___LISTAS_H___
 #define ___LISTAS_H___
 
 #include "dados.h"
 
+/**
+ * \brief Refere-se à estrutura para as listas ligadas.
+ */
 typedef struct slist *LISTA;
 
 typedef struct slist {
@@ -11,11 +18,44 @@ typedef struct slist {
     LISTA prox;
 } Nodo;
 
+/**
+ * \brief Função que cria uma lista vazia.
+ * @return Uma lista l vazia
+ */
 LISTA criar_lista();
+
+/**
+ * \brief Função que insere um valor na cabeça da lista.
+ * @param L Lista
+ */
 LISTA insere_cabeca(LISTA L, void *valor);
+
+/**
+ * \brief Função que devolve a cabeça da lista.
+ * @param L Lista
+ * @return Cabeça da lista
+ */
 void * devolve_cabeca(LISTA L);
+
+/**
+ * \brief Função que devolve a cauda da lista.
+ * @param L Lista
+ * @return Cauda da lista
+ */
 LISTA proximo(LISTA L);
+
+/**
+ * \brief Função que remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda.
+ * @param L Lista
+ * @return Cauda da lista
+ */
 LISTA remove_cabeca(LISTA L);
+
+/**
+ * \brief Função que nos indica se a lista é vazia.
+ * @param L Lista
+ * @return 1 se verdadeiro ou 0 se falso
+ */
 int lista_esta_vazia(LISTA L);
 
 #endif //___LISTAS_H___
