34d713337714dbe57e09e7336ebee4f58dfac755
AdelioJoseFerreiraFernandes <a78778@alunos.uminho.pt>
2020-04-27 11:49:36 +0100

    otimização de código

--- git/dados.c
+++ git/dados.c
@@ -3,11 +3,13 @@
 #include "dados.h"
 #include "listas.h"
 
-ESTADO *inicializar_estado() {
+ESTADO *inicializar_estado()
+{
     ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
     e->jogador_atual = 1;
     e->num_jogadas = 0;
-    int l,c;
+    int l, c;
+
     for (l = 0; l <= 7; l++) {
         for (c = 0; c<=7; c++) {
             e->tab[l][c]= VAZIO;
@@ -20,71 +22,97 @@ ESTADO *inicializar_estado() {
     return e;
 }
 
-int obter_jogador_atual(ESTADO *estado) {
+int obter_jogador_atual(ESTADO *estado)
+{
     return (*estado).jogador_atual;
 }
-int obter_numero_de_jogadas(ESTADO *estado) {
+
+int obter_numero_de_jogadas(ESTADO *estado)
+{
     return (*estado).num_jogadas;
 }
-CASA obter_estado_casa(ESTADO *e, COORDENADA c) {
-    int lin = c.linha;
-    int col = c.coluna;
+
+CASA obter_estado_casa(ESTADO *e, COORDENADA c)
+{
+    int lin, col;
+    lin = c.linha;
+    col = c.coluna;
     CASA casa = e->tab[lin][col];
     return casa;
 }
 
-COORDENADA obter_ultima_jogada(ESTADO *e) {
+COORDENADA obter_ultima_jogada(ESTADO *e)
+{
     return e->ultima_jogada;
 }
 
-int obter_num_comandos(ESTADO *e) {
+int obter_num_comandos(ESTADO *e)
+{
     return (*e).num_comandos;
 }
 
-void altera_para_branca (ESTADO *e, COORDENADA c) {
+void altera_para_branca (ESTADO *e, COORDENADA c)
+{
     e->tab[c.linha][c.coluna]=BRANCA;
 }
 
-void altera_para_preta (ESTADO *e, COORDENADA c) {
+void altera_para_preta (ESTADO *e, COORDENADA c)
+{
     e->tab[c.linha][c.coluna]=PRETA;
 }
 
-void altera_para_vazio (ESTADO *e, COORDENADA c) {
+void altera_para_vazio (ESTADO *e, COORDENADA c)
+{
     e->tab[c.linha][c.coluna]=VAZIO;
 }
 
-void atualiza_ultima_jogada (ESTADO *e, COORDENADA c) {
+void atualiza_ultima_jogada (ESTADO *e, COORDENADA c)
+{
     e->ultima_jogada = c;
 }
 
-void atualiza_jog_atual (ESTADO *e) {
-    int n = obter_jogador_atual(e);
-    if (n == 1) e->jogador_atual = 2;
-    else e->jogador_atual = 1;
+void atualiza_jog_atual (ESTADO *e)
+{
+    int n;
+    n = obter_jogador_atual(e);
+
+    if (n == 1)
+        e->jogador_atual = 2;
+    else
+        e->jogador_atual = 1;
 }
 
-void adic_num_comandos (ESTADO *e) {
+void adic_num_comandos (ESTADO *e)
+{
     e->num_comandos += 1;
 }
 
-void adic_num_jogadas (ESTADO *e) {
-    int j = obter_jogador_atual(e);
-    if (j == 2) e->num_jogadas += 1;
+void adic_num_jogadas (ESTADO *e)
+{
+    int j;
+    j = obter_jogador_atual(e);
+
+    if (j == 2)
+        e->num_jogadas += 1;
 }
 
-void subt_num_jogadas (ESTADO *e) {
+void subt_num_jogadas (ESTADO *e)
+{
     e->num_jogadas -= 1;
 }
 
-void altera_prompt (ESTADO *e, int nc, int nj, int ja) {
+void altera_prompt (ESTADO *e, int nc, int nj, int ja)
+{
     e->num_jogadas = nj;
     e->num_comandos = nc;
     e->jogador_atual = ja;
 
 }
 
-void altera_tabuleiro (ESTADO *e, char d, COORDENADA cord) {
-    if (d == '#') altera_para_preta(e,cord);
+void altera_tabuleiro (ESTADO *e, char d, COORDENADA cord)
+{
+    if (d == '#')
+        altera_para_preta(e,cord);
     else if (d == '*') {
         altera_para_branca(e,cord);
         atualiza_ultima_jogada(e,cord);
@@ -92,25 +120,34 @@ void altera_tabuleiro (ESTADO *e, char d, COORDENADA cord) {
     else altera_para_vazio(e,cord);
 }
 
-void adic_jogadas (ESTADO *e, COORDENADA c){
-    int n = obter_numero_de_jogadas(e);
-    int j = obter_jogador_atual(e);
-    if (j == 1) e->jogadas[n].jogador1 = c;
-    else e->jogadas[n].jogador2 = c;
+void adic_jogadas (ESTADO *e, COORDENADA c)
+{
+    int n, j;
+    n = obter_numero_de_jogadas(e);
+    j = obter_jogador_atual(e);
+
+    if (j == 1)
+        e->jogadas[n].jogador1 = c;
+    else
+        e->jogadas[n].jogador2 = c;
 }
 
-JOGADA obter_jogada (ESTADO *e, int i){
+JOGADA obter_jogada (ESTADO *e, int i)
+{
     return e->jogadas[i];
 }
 
-void start_jogador (ESTADO *e){
+void start_jogador (ESTADO *e)
+{
     e->jogador_atual=1;
 }
 
-void start_num_jogadas (ESTADO *e){
+void start_num_jogadas (ESTADO *e)
+{
     e->num_jogadas=0;
 }
 
-COORDENADA devolve_coordenada (LISTA l) {
+COORDENADA devolve_coordenada (LISTA l)
+{
     return *(COORDENADA *) devolve_cabeca(l);
 }
--- git/logica.c
+++ git/logica.c
@@ -5,14 +5,17 @@
 #include <time.h>
 
 
-int jogada_invalida (ESTADO *e, COORDENADA c){
+int jogada_invalida (ESTADO *e, COORDENADA c)
+{
     if (c.linha >= 0 && c.linha <= 7 && c.coluna >= 0 && c.coluna <= 7 ) {
         if (obter_estado_casa(e,c)==PRETA) return 1;
         if (obter_estado_casa(e,c)==BRANCA) return 1;
 
-        COORDENADA uj = obter_ultima_jogada(e);
-        int diflinha = uj.linha - c.linha;
-        int difcoluna = uj.coluna - c.coluna;
+        COORDENADA uj;
+        int diflinha, difcoluna;
+        uj = obter_ultima_jogada(e);
+        diflinha = uj.linha - c.linha;
+        difcoluna = uj.coluna - c.coluna;
 
         if ((abs(diflinha) > 1) || (abs(difcoluna) > 1)) return 1;
         return 0;
@@ -20,7 +23,8 @@ int jogada_invalida (ESTADO *e, COORDENADA c){
     else return 1;
 }
 
-void aux_jog_poss (COORDENADA *c, int i) {
+void aux_jog_poss (COORDENADA *c, int i)
+{
     if (i == 0) (*c).linha += 1;
     if (i == 1) (*c).coluna += 1;
     if (i == 2 || i == 3) (*c).linha -= 1;
@@ -29,8 +33,10 @@ void aux_jog_poss (COORDENADA *c, int i) {
 }
 
 
-int ha_jogadas_possiveis (ESTADO *e){
-    COORDENADA c = obter_ultima_jogada(e);
+int ha_jogadas_possiveis (ESTADO *e)
+{
+    COORDENADA c;
+    c = obter_ultima_jogada(e);
     for(int i = 0; i <= 7; i++) {
         aux_jog_poss(&c,i);
         if (jogada_invalida(e, c) == 0) return 1;
@@ -38,31 +44,36 @@ int ha_jogadas_possiveis (ESTADO *e){
     return 0;
 }
 
-int acabou (ESTADO *e) {
+int acabou (ESTADO *e)
+{
     if (ha_jogadas_possiveis(e) == 1){
         COORDENADA c1, c2;
         c1.linha = 0; c1.coluna = 7;
         c2.linha = 7; c2.coluna = 0;
-        if (obter_estado_casa(e,c1)==BRANCA || obter_estado_casa(e,c2)==BRANCA) return 1;
-        else return 0;
+        if (obter_estado_casa(e,c1)==BRANCA || obter_estado_casa(e,c2)==BRANCA)
+            return 1;
+        else
+            return 0;
     }
     return 1;
 }
 
-int quem_ganhou (ESTADO *e) {
+int quem_ganhou (ESTADO *e)
+{
     COORDENADA c;
     c.linha = 0; c.coluna = 7;
-    if (obter_estado_casa(e,c)==BRANCA) return 2;
-    c.linha = 7; c.coluna = 0;
-    if (obter_estado_casa(e,c)==BRANCA) return 1;
-    if (obter_jogador_atual(e)==1) return 2;
-    else return 1;
+    if (obter_estado_casa(e,c)==BRANCA || obter_jogador_atual(e)==1)
+        return 2;
+    else
+        return 1;
 }
 
 
-int jogar(ESTADO *e, COORDENADA c) {
+int jogar(ESTADO *e, COORDENADA c)
+{
+    COORDENADA uj;
     if (jogada_invalida(e,c) != 1) {
-        COORDENADA uj = obter_ultima_jogada(e);
+        uj = obter_ultima_jogada(e);
         altera_para_branca(e,c);
         altera_para_preta(e,uj);
         atualiza_ultima_jogada(e,c);
@@ -74,58 +85,75 @@ int jogar(ESTADO *e, COORDENADA c) {
     return 1;
 }
 
-COORDENADA heuristica_aleatoria (LISTA l) {
+COORDENADA heuristica_aleatoria (LISTA l)
+{
+    int p;
+    COORDENADA c;
+
     srand(time(NULL));
-    int p = rand () % contaLista(l);
-    COORDENADA c = *(COORDENADA *) procuraNaLista(l,p);
+    p = rand () % contaLista(l);
+    c = *(COORDENADA *) procuraNaLista(l,p);
     return c;
 }
 
-COORDENADA heuristica_euclidiana (ESTADO *e, LISTA l)  {
+COORDENADA heuristica_euclidiana (ESTADO *e, LISTA l)
+{
     int valor, melhores_pontos, pontos;
     COORDENADA c, melhor_jogada;
 
-    if (obter_jogador_atual(e) == 1) {
+    if (obter_jogador_atual(e) == 1)
+    {
         valor = 1;
         melhores_pontos = 15;
     }
-    else {
+    else
+    {
         valor = -1;
         melhores_pontos = 1;
     }
-    while (lista_esta_vazia(l) != 1) {
+    while (lista_esta_vazia(l) != 1)
+    {
         c = devolve_coordenada(l);
         pontos = ((7-c.linha) + c.coluna) * valor;
-        if (pontos < melhores_pontos) {
+        if (pontos < melhores_pontos)
+        {
             melhores_pontos = pontos;
             melhor_jogada = c;
         }
         l = remove_cabeca(l);
-
     }
     return melhor_jogada;
 }
 
-LISTA obtem_jogadas_possiveis (ESTADO *e) {
+LISTA obtem_jogadas_possiveis (ESTADO *e)
+{
     LISTA l = criar_lista();
     COORDENADA c = obter_ultima_jogada(e);
     COORDENADA v[sizeof(COORDENADA)*8];
-    for(int i = 0; i <= 7; i++) {
+
+    for(int i = 0; i <= 7; i++)
+    {
         aux_jog_poss (&c,i);
-        if (jogada_invalida(e, c) == 0) {
+        if (jogada_invalida(e, c) == 0)
+        {
             v[i] = c;
             l = insere_cabeca(l,(void *) &v[i]);
         }
     }
     return l;
 }
-void regressa_pos (ESTADO *e, int p) {
-    int nj = obter_numero_de_jogadas(e);
+void regressa_pos (ESTADO *e, int p)
+{
+    int nj;
+    COORDENADA uc;
+    nj = obter_numero_de_jogadas(e);
+
     if (p <= nj){
-        COORDENADA uc = obter_ultima_jogada(e);
+        uc = obter_ultima_jogada(e);
         altera_para_vazio(e,uc);
         if (obter_jogador_atual(e) == 2) atualiza_jog_atual(e);
-        for (; nj >= p; nj--) {
+        for (; nj >= p; nj--)
+        {
             JOGADA j = obter_jogada(e, nj-1);
             COORDENADA j1 = j.jogador1;
             COORDENADA j2 = j.jogador2;
